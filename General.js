98aab4a71a11c0c361fb7906ddeaf2a2a5552423
bytebusterspages <Bytebusters.contacto@gmail.com>
2024-08-03 18:01:15 -0500

    Add files via upload

---
+++ General.js
@@ -0,0 +1,22 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("mySidebar");
+    if (sidebar.style.width === "250px")
+        sidebar.style.width = "0";
+    else (
+        sidebar.style.width = "250px"
+    )
+}
+
+
+function toggleSidebar2() {
+    var sidebar = document.getElementById("mySidebar2");
+    if (sidebar.style.width === "250px")
+        sidebar.style.width = "0";
+    
+}
+
+
+
+
+
+AOS.init();
