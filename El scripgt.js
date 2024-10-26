318de2ef1b6351f68d29368b623c89ae8955d0f5
bytebusterspages <Bytebusters.contacto@gmail.com>
2024-10-26 17:34:44 -0500

    Add files via upload

---
+++ El scripgt.js
@@ -0,0 +1,10 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("mySidebar");
+    if (sidebar.style.width === "250px") 
+        sidebar.style.width = "0";
+    else (
+        sidebar.style.width = "250px"
+)
+}
+AOS.init();
+
